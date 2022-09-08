014a9f6286fcfeed7ebfa7457b9931d293455a91
karun <karunakarreddy1305@gmil.com>
2022-09-08 14:50:12 +0530

    print('who the hell r u')

--- main.py
+++ main.py
@@ -7,10 +7,16 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def add(a,b):
+    return (a+b)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
     print('who the hell r u')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(add(2,3))
+    q=1
+    w=2
+    e=q+w
+    print(e)
+# See PyCharm help at https://www.jetbrains.com/help/pycha
